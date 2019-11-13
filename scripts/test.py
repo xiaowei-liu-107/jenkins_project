ad8d761de8ccf33fa793b0ecd900f1c0ec4706e6
xiaowei <1074935947@qq.com>
2019-11-13 18:54:42 +0800

    first commit

--- scripts/test.py
+++ scripts/test.py
@@ -11,9 +11,11 @@ class TestContact:
         print("123")
 
 
+
     def test_login2(self):
         print("123")
 
+        assert 0
 if __name__ == '__main__':
     # pytest.main("-s login.py")
     pytest.main(["-s",  "test.py"])
